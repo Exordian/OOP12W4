603d042d7ece1dda714857a5f7b91d27a78e9a9f
Flo <Flo@FlosLaptop>
2012-11-07 13:27:41 +0100

    klassen erstellt

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * @author Englisch (e1125164), Lenz (e1126963), Schuster (e1025700) 
+ * @since November 2012
+ * 
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// trolo
+
+	}
+
+}
